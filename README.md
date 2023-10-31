f3a5f80ac43c4f720e069601b8ec2c70b6925cab
CodeSandbox <support@codesandbox.io>
2023-10-31 14:53:58 +0000

    Sandbox snapshot commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vanilla-template
+
+This is a template for weekly assignments.
---
+++ src/index.js
@@ -0,0 +1,47 @@
+import "./styles.css";
+
+if (document.readyState !== "loading") {
+  initializeCode();
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    initializeCode();
+  });
+}
+
+function initializeCode() {
+  document.getElementById("app").innerHTML = "<h1>Hello!</h1>";
+}
+
+function createElement(){
+  let wikiItem = document.createElement("div");
+
+  wikiItem.innerHTML = `
+    <h1 class="wiki-header">Breed X</h1>
+    <div class="wiki-content">
+      <p class="wiki-text">
+        Some text about this breed.
+      </p>
+      <div class="img-container">
+        <img class="wiki-img" src="">
+      </div>
+    </div>
+  `;
+
+  wikiItem.className = "wiki-item";
+  return wikiItem;
+}
+
+function appendChild() {
+  
+  let item = createElement();
+  
+
+  const tBody = document.getElementById("app");
+
+  tBody.appendChild(item)
+
+}
+
+for (let i = 0; i<5; i++){
+  appendChild();
+}
